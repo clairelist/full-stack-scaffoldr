05293813327999e2337f483ae2900aba8d1f9b77
Claire List <claire.list2501@gmail.com>
2022-01-24 14:12:19 -0700

    model built

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,29 +1,42 @@
+const lego = require('../../data/db-config');
+
 /**
   resolves to an ARRAY with all users, each user having { user_id, username }
  */
 function find() {
-
+  return lego('users').select('id', 'username');
 }
 
 /**
   resolves to an ARRAY with all users that match the filter condition
  */
 function findBy(filter) {
-
+  return lego('users').where(filter);
 }
 
 /**
   resolves to the user { user_id, username } with the given user_id
  */
 function findById(user_id) {
-
+  return lego('users')
+    .where({ user_id })
+    .first();
 }
 
 /**
   resolves to the newly inserted user { user_id, username }
  */
-function add(user) {
-
-}
+  async function add(user) {
+    const [id] = await lego('users').insert(user);
+  
+    return findById(id);
+  }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
+
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById,
+}
