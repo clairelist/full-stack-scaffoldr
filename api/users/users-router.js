b35c9482e70d0332013fcfa047b839ce431c9797
Claire List <claire.list2501@gmail.com>
2022-02-16 20:50:08 -0700

    del built

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -10,4 +10,19 @@ const Users = require("./users-model");
       .catch(next)
   })
   
+  //router DELETE requesty !
+  //POST to '/delete_account', supply user id. Only accessable to logged in users !
+  // --> MAYBE ADD: delete session token here, too.
+  router.post('/delete_account', restricted, (req,res,next) => {
+    //call Users.deleteById, passing in the id !
+    const {user_id} = req.params.id; //let's write a test that this works !!
+    Users.deleteById(user_id)
+      .then(()=>{
+        res.status(200).json({message: 'Your account was succesfully deleted!'})
+      }).catch(err=>{
+        next(err);
+      })
+  })
+
+  //TODOS:: BUILD A CATCHALL ENDPOINT !
   module.exports = router;
